7e20b415c2e5adf46dfad72d4b5a28679e7e8ff3
Rawkesh2k <rakeshkumarkurupati@gmail.com>
2024-10-23 20:46:47 -0400

    adding spring security - starting with adding entities and creating tables with joins

---
+++ src/main/java/com/springMVC/mvc/models/Role.java
@@ -0,0 +1,26 @@
+package com.springMVC.mvc.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<UserEntity> users = new ArrayList<>();
+}
